21fd1d1197db4dfdb72ec1b8a90510ce0afa9c73
andrew-fisher1 <113075629+andrew-fisher1@users.noreply.github.com>
2024-04-09 12:28:23 -0400

    4/8/24 updates

--- initial.py
+++ initial.py
@@ -7,6 +7,110 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
+key_dict = {
+"Baltimore Ravens" : "Baltimore",
+"Baltimore" : "Baltimore",
+
+"Cleveland Browns": "Cleveland",
+"Cleveland": "Cleveland",
+
+"Pittsburgh Steelers": "Pittsburgh",
+"Pittsburgh": "Pittsburgh",
+
+"Cincinnati Bengals": "Cincinnati",
+"Cincinnati": "Cincinnati",
+
+"Buffalo Bills": "Buffalo",
+"Buffalo": "Buffalo",
+
+"Miami Dolphins": "Miami",
+"Miami": "Miami",
+
+"New York Jets": "New York Jets",
+"NY Jets": "New York Jets",
+
+"New England Patriots": "New England",
+"New England": "New England",
+
+"Kansas City Chiefs": "Kansas City",
+"Kansas City": "Kansas City",
+
+"Oakland Raiders": "Las Vegas",
+"Las Vegas": "Las Vegas",
+"Las Vegas Raiders": "Las Vegas",
+
+"Denver Broncos": "Denver",
+"Denver": "Denver",
+
+"San Diego Chargers": "Los Angeles Chargers",
+"Los Angeles Chargers": "Los Angeles Chargers",
+"LA Chargers": "Los Angeles Chargers",
+
+"Houston Texans": "Houston",
+"Houston": "Houston",
+
+"Jacksonville Jaguars": "Jacksonville",
+"Jacksonville": "Jacksonville",
+
+"Tennessee Titans": "Tennessee",
+"Tennessee": "Tennessee",
+
+"Indianapolis Colts" : "Indianapolis",
+"Indianapolis" : "Indianapolis",
+
+"San Francisco 49ers": "San Francisco",
+"San Francisco": "San Francisco",
+
+"St. Louis Rams": "Los Angeles Rams",
+"Los Angeles Rams": "Los Angeles Rams",
+"LA Rams": "Los Angeles Rams",
+
+"Arizona Cardinals": "Arizona",
+"Arizona": "Arizona",
+
+"Seattle Seahawks": "Seattle",
+"Seattle": "Seattle",
+
+"Dallas Cowboys": "Dallas",
+"Dallas": "Dallas",
+
+"Washington Football Team": "Washington",
+"Washington Redskins": "Washington",
+"Washington Commanders": "Washington",
+"Washington": "Washington",
+
+"Philadelphia Eagles": "Philadelphia",
+"Philadelphia": "Philadelphia",
+
+"New York Giants": "New York Giants",
+"NY Giants": "New York Giants",
+
+"Detroit Lions": "Detroit",
+"Detroit": "Detroit",
+
+"Chicago Bears": "Chicago",
+"Chicago": "Chicago",
+
+"Green Bay Packers": "Green Bay",
+"Green Bay": "Green Bay",
+
+"Minnesota Vikings": "Minnesota",
+"Minnesota": "Minnesota",
+
+"Carolina Panthers": "Carolina",
+"Carolina": "Carolina",
+
+"New Orleans Saints": "New Orleans",
+"New Orleans": "New Orleans",
+
+"Atlanta Falcons": "Atlanta",
+"Atlanta": "Atlanta",
+
+"Tampa Bay Buccaneers": "Tampa Bay",
+"Tampa Bay": "Tampa Bay",
+
+}
+
 # get_player_name
 # Parameters: Soup object
 # Purpose: Extracts and returns the team name from the provided BeautifulSoup 
@@ -18,30 +122,6 @@ from bs4 import BeautifulSoup
 
 #     player_name = player_name_container.find("span").text.strip()
 #     return player_name
-
-
-# get_stat_given_win
-# Parameters: A list of rows indicating games where a team led in yards per 
-#             passing attempt and a url
-# Purpose: To find the probability of leading passing yards per attempt given 
-#   that you win while also finding the probability that a team wins in general
-def get_one_year(soup):
-    team_names = {}
-    #print(soup)
-    schedule_table = soup.find(class_="tr-table datatable scrollable")
-
-    rows = schedule_table.find_all('tr')
-
-    for row in rows:
-        name_location = row.find('td', {"class" : "text-left nowrap"})
-        percent_location = row.find('td', {"class" : "text-right"})
-        
-        if name_location is not None and percent_location is not None:
-            name = name_location["data-sort"]
-            percent = percent_location["data-sort"]
-            team_names[name] = percent
-
-    return team_names
     
 
 # get_game_urls
@@ -157,21 +237,103 @@ def get_one_year(soup):
 #                 return team_name_location.text.strip()
 
 #     return None
+def get_rushing_pcts():
+    toReturn = {}
+    for i in range (21):
+        year = str(2024 - i)
+        url = "https://www.teamrankings.com/nfl/stat/rushing-play-pct?date=" + year + "-03-1"
+
+        
+        soup = BeautifulSoup(requests.get(url).content, "html.parser")
+        toReturn[int(year)] = get_one_year(soup)
+    return toReturn
+
+
+def get_one_year(soup):
+    team_percents = {}
+    #print(soup)
+    schedule_table = soup.find(class_="tr-table datatable scrollable")
+
+    rows = schedule_table.find_all('tr')
+
+    for row in rows:
+        name_location = row.find('td', {"class" : "text-left nowrap"})
+        percent_location = row.find('td', {"class" : "text-right"})
+        
+        if name_location is not None and percent_location is not None:
+            name = name_location["data-sort"]
+            percent = percent_location["data-sort"]
+            team_percents[name] = dict(pct = percent, outcome = 0)
+
+    return team_percents
+
+
+
+def get_tables(url):
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
+    tables = soup.find_all(class_ = "wikitable sortable")
+    wild_card = tables[0]
+    divisional = tables[1]
+    conference = tables[2]
+   
+    return wild_card, divisional, conference
+
+def wild_card_points(wild_card, teams):
+    rows = wild_card.find('tbody').find_all('tr')
+
+
+# Only works because the rows without years have an a and then a b
+    for i in range(len(rows)):
+       row = rows[i]
+       if row:
+            yeartd = row.find('td')
+            if yeartd:
+                yearb = yeartd.find('b')
+                if yearb:
+                    year_location =  yearb.find('a')
+                    if year_location:
+                        year = year_location.text
+                        if int(year[-2:]) > 24 or int(year[-2:]) < 4:
+                            continue
+                        year = 2000 + int(year[-2:])
+                        rowspanVal = int(yeartd['rowspan'])
+                        get_div_results_for_year(rows, i, rowspanVal, year, teams)
+
+
+def get_div_results_for_year(rows, index, rowSpanVal, year, teams):
+    for i in range (index, index + rowSpanVal):
+        offset = 0 # used to figure out if we are in the first column
+        row = rows[i]
+        cols = row.find_all("td")
+        if i == index:
+            offset = 1
+        # loop through each year and get each team from the table 
+        for j in range (0 + offset, 7 + offset, 2):
+            team_col = cols[j]
+            name_location = team_col.find("a")
+            # if name_location.text == None:
+            #     name = name_location.find("b").text
+            # else:
+            #     name = name_location.text
+            if name_location:
+                name = name_location.text.lstrip()
+                print(name)
+        
 
 def main():
     # Below are just other urls you could run this on
     # BEWARE the rate limited request
     full_dict = {}
     # KEYS ARE THE LAST YEAR OF THE SEASON E.G 2023-2024 is coded as 2024
-    for i in range (21):
-        year = str(2024 - i)
-        url = "https://www.teamrankings.com/nfl/stat/rushing-play-pct?date=" + year + "-03-1"
-
-        # Get and print the team name
-        soup = BeautifulSoup(requests.get(url).content, "html.parser")
-        full_dict[year] = get_one_year(soup)
+    #full_dict = get_rushing_pcts()
         
     print (full_dict)
+
+    
+    results_url = "https://en.wikipedia.org/wiki/NFL_playoff_results"
+    wild_card, divisional, conference = get_tables(results_url)
+    wild_card_points(wild_card, full_dict)
 #     team_name   = get_team_name(soup)
 
 #     if player_name and team_name:
