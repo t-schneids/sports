9b9acaee9ac3e1808745e86bc03555919ff3c136
t-schneids <tschneids1@icloud.com>
2024-04-24 16:41:03 -0400

    Creating playoff result class

--- prediction.py
+++ prediction.py
@@ -3,6 +3,7 @@
 # Program to analyze the efficiency of NFL teams with their chosen run 
 # percentage based on how far they made it into the playoffs. 
 
+from playoffResults import PlayoffResults
 import requests
 import re
 from bs4 import BeautifulSoup
@@ -215,94 +216,6 @@ def get_opp_rushing_per_attempt(sched_dict):
     return toReturn
 
 
-def get_tables(url):
-    soup = BeautifulSoup(requests.get(url).content, "html.parser")
-
-    tables = soup.find_all(class_ = "wikitable sortable")
-    wild_card = tables[0]
-    divisional = tables[1]
-    conference = tables[2]
-   
-    return wild_card, divisional, conference
-
-
-# Get wild card and divisional points for the dictionary
-def wild_card_divisional_points(wild_card, teams, isDivisional):
-    rows = wild_card.find('tbody').find_all('tr')
-
-# Only works because the rows without years have an a and then a b
-    for i in range(len(rows)):
-       row = rows[i]
-       if row:
-            yeartd = row.find('td')
-            if yeartd:
-                yearb = yeartd.find('b')
-                if yearb:
-                    year_location =  yearb.find('a')
-                    if year_location:
-                        year = year_location.text
-                        if int(year[-2:]) > 24 or int(year[-2:]) < 4:
-                            continue
-                        year = 2000 + int(year[-2:])
-                        rowspanVal = int(yeartd['rowspan'])
-                        get_wild_results_for_year(rows, i, rowspanVal, year, teams, isDivisional)
-
-# This gets wild card and divisional results for a year despite the name
-def get_wild_results_for_year(rows, index, rowSpanVal, year, teams, 
-                              isDivisional):
-    for i in range (index, index + rowSpanVal):
-        offset = 0 # used to figure out if we are in the first column
-        row = rows[i]
-        cols = row.find_all("td")
-        if i == index:
-            offset = 1
-
-        # loop through each year and get each team from the table 
-        for j in range (0 + offset, 7 + offset, 2):
-            team_col = cols[j]
-            name_location = team_col.find("a")
-            if name_location:
-                name = name_location.text.lstrip()
-                realName = key_dict[name]
-                if isDivisional:
-                    teams[year][realName]['outcome'] = 4
-                   
-                else: 
-                    if year < 2020:
-                        teams[year][realName]['outcome'] = 2 
-                    else:
-                        teams[year][realName]['outcome'] = 2
-
-
-
-def conference_superBowl_points(conference, teams):
-    rows = conference.find('tbody').find_all('tr')
-
-# Only works because the rows without years have an a and then a b
-    for i in range(len(rows)):
-       row = rows[i]
-       if row:
-            yeartd = row.find('td')
-            if yeartd:
-                yearb = yeartd.find('b')
-                if yearb:
-                    year_location =  yearb.find('a')
-                    if year_location:
-                        year = year_location.text
-                        if int(year[-2:]) > 24 or int(year[-2:]) < 4:
-                            continue
-                        year = 2000 + int(year[-2:])
-                        get_div_champ_results_for_year(row, year, teams)
-
-# def score_outcomes(year, teams, team1, team2, scores):
-#     rTeam1 = key_dict[team1]
-#     rTeam2 = key_dict[team2]
-#     if int(scores[0]) > int(scores[1]):
-#         teams[year][rTeam1]['outcome'] = 8
-#         teams[year][rTeam2]['outcome'] = 4
-#     else: 
-#         teams[year][rTeam1]['outcome'] = 4
-#         teams[year][rTeam2]['outcome'] = 8
 def make_sched_dict():
     team_schedules = {}
     for i in range (21):
@@ -339,44 +252,6 @@ def make_sched_dict():
     return team_schedules
 
 
-def get_div_champ_results_for_year(row, year, teams):
-    cols = row.find_all("td")
-    
-    team1 = cols[1].find('a').text.lstrip()
-    teams[year][key_dict[team1]]['outcome'] = 16
-    
-    # won the superbowl
-    if cols[1].find('b') != None:
-        teams[year][key_dict[team1]]['outcome'] = 32
-
-
-    team2 = cols[3].find('a').text.lstrip()
-    teams[year][key_dict[team2]]['outcome'] = 8
-    # score = cols[2]
-    # match = re.search(r'(\d+)\D+(\d+)', score.text)
-    # print(match.group(1))
-    # score_outcomes(year, teams, team1, team2, [int(match.group(1)), 
-                                            #    int(match.group(2))])
-
-    team3 = cols[5].find('a').text.lstrip()
-    teams[year][key_dict[team3]]['outcome'] = 16
-    
-    # won the superbowl
-    if cols[5].find('b') != None:
-        teams[year][key_dict[team3]]['outcome'] = 32
-
-
-    team4 = cols[7].find('a').text.lstrip()
-    teams[year][key_dict[team4]]['outcome'] = 8
-
-
-
-
-    # score2 = cols[6]
-    # scores2 = score2.text.split('-')
-    # match2 = re.search(r'(\d+)\D+(\d+)', score2.text)
-    # score_outcomes(year, teams, team3, team4, [int(match2.group(1)), 
-                                            #    int(match2.group(2))])
 def plot(full_dict):
     x_rushpcts = []
     y_outcomes = []
@@ -436,14 +311,13 @@ def main():
     # full_dict = add_stats_to_dict(full_dict, get_opp_rushing_per_attempt(sched_dict), "opp_ypc")
 
 
-    # results_url = "https://en.wikipedia.org/wiki/NFL_playoff_results"
-    # wild_card, divisional, conference = get_tables(results_url)
-    # wild_card_divisional_points(wild_card, full_dict, False)
-    # wild_card_divisional_points(divisional, full_dict, True)
-    # conference_superBowl_points(conference, full_dict)
-    with open('data_dict', 'rb') as file:
-        full_dict = pickle.load(file)
+   
+    # with open('data_dict', 'rb') as file:
+    #     full_dict = pickle.load(file)
 
+
+    playoffResultsObj = PlayoffResults(key_dict)
+    full_dict = playoffResultsObj.get_playoff_results({})
     print(full_dict)
     
     # with open('data_dict', 'wb') as fp:
