42316f4f6ae8b17faac1aa40155d35e4816d21e2
andrew-fisher1 <113075629+andrew-fisher1@users.noreply.github.com>
2024-04-25 12:54:07 -0400

    models in seperate file

--- prediction.py
+++ prediction.py
@@ -8,9 +8,9 @@ import requests
 from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import numpy as np
+from models import *
 import pickle
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression
+
 
 
 key_dict = {
@@ -285,74 +285,6 @@ def make_label_arr(full_dict):
             outerList.append(dict[year_dict[team]['outcome']])
             
     return np.array(outerList)
-
-def train_test_eval_linear(xs, ys):
-    yearAccs = []
-    yearPlayoffCorrect = []
-    for i in range (21):
-        model = LinearRegression()
-        rangelow = i * 32
-        rangehi = (i * 32) + 32
-        xtrain = np.concatenate([xs[:rangelow], xs[rangehi:]], axis=0)
-        ytrain = np.concatenate([ys[:rangelow], ys[rangehi:]], axis=0)
-        x_test = xs[rangelow:rangehi]
-        y_test = ys[rangelow:rangehi]
-        model.fit(np.array(xtrain), np.array(ytrain))
-        y_pred = model.predict(x_test)
-        # print(y_pred)
-        avg = 0
-        num_playoff_correct = 0
-        num_false_negs = 0
-        for i in range (0, 32):
-            if y_test[i] > 1:
-                if y_pred[i] > 1.5:
-                    num_playoff_correct += 1
-            if y_pred[i] > 1.5:
-                if y_test[i] == 1:
-                    num_false_negs += 1
-            diff = abs(y_pred[i] - y_test[i])
-            avg += diff
-
-        yearPlayoffCorrect.append(num_playoff_correct)
-
-        yearAccs.append(avg / 32)
-
-
-
-
-    return yearAccs
-
-def train_test_eval_trees(xs, ys):
-    yearAccs = []
-    yearPlayoffCorrect = []
-    for i in range (21):
-        model = RandomForestClassifier(class_weight='balanced')
-        rangelow = i * 32
-        rangehi = (i * 32) + 32
-        xtrain = np.concatenate([xs[:rangelow], xs[rangehi:]], axis=0)
-        ytrain = np.concatenate([ys[:rangelow], ys[rangehi:]], axis=0)
-        x_test = xs[rangelow:rangehi]
-        y_test = ys[rangelow:rangehi]
-        model.fit(np.array(xtrain), np.array(ytrain))
-        y_pred = model.predict(x_test)
-        avg = 0
-        num_playoff_correct = 0
-        num_false_negs = 0
-        for i in range (0, 32):
-            if y_test[i] > 1:
-                if y_pred[i] > 1.5:
-                    num_playoff_correct += 1
-            if y_pred[i] > 1.5:
-                if y_test[i] == 1:
-                    num_false_negs += 1
-            diff = abs(y_pred[i] - y_test[i])
-            avg += diff
-
-        yearPlayoffCorrect.append(num_playoff_correct)
-
-        yearAccs.append(avg / 32)
-
-    return yearPlayoffCorrect
 
 def main():
     # Below are just other urls you could run this on
